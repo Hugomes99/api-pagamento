a41f760ef39a6e87968837d1f9fcf5f2c46683f6
Hugomes99 <hugochristian99@gmail.com>
2023-09-28 18:21:58 -0300

    API-Pagamentos

---
+++ src/test/java/com/hugo/ApiPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
