5a8cccb813f909c2cf7108c4156d5ac3420df3a4
Hugomes99 <hugochristian99@gmail.com>
2023-09-28 22:22:15 -0300

    Repository da API de pagamento

--- src/main/java/com/hugo/Repository/PagamentoRepository.java
+++ src/main/java/com/hugo/Repository/PagamentoRepository.java
@@ -1,5 +1,11 @@
-package com.hugo.Repository;
+package com.hugo.repository;
 
-public class PagamentoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hugo.model.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
 
 }
