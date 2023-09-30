78db59591f6a7a2edde1065b207f47b82f0d2697
Hugomes99 <hugochristian99@gmail.com>
2023-09-29 22:07:45 -0300

    Ajustes no model

--- src/main/java/com/hugo/Model/PagamentoTipo.java
+++ src/main/java/com/hugo/Model/PagamentoTipo.java
@@ -2,8 +2,8 @@ package com.hugo.model;
 
 public final class PagamentoTipo {
     public static final String PG_PIX = "pix";
-    public static final String PG_CARTAO_CREDITO = "cartao-credito";
-    public static final String PG_CARTAO_DEBITO = "cartao-debito";
+    public static final String PG_CARTAO_CREDITO = "cartao_credito";
+    public static final String PG_CARTAO_DEBITO = "cartao_debito";
     public static final String PG_BOLETO = "boleto";
 
     private PagamentoTipo() {
