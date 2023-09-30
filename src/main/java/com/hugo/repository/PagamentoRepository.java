65aed7ee66b928228b62053928e091aed461df9b
moises.costa <moises.costa@leadstation.com.br>
2023-09-30 11:01:11 -0300

    - mudar o nome das pasta, todos as pastas devem ter as letras minusculas - colocar os métodos de consulta personalizados

--- src/main/java/com/hugo/repository/PagamentoRepository.java
+++ src/main/java/com/hugo/repository/PagamentoRepository.java
@@ -1,5 +1,7 @@
 package com.hugo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.hugo.model.Pagamento;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+    List<Pagamento> findByCpfCnpj(String cpfCnpj);
+
+    List<Pagamento> findByStatus(String pagamentoStatus);
 
 }
