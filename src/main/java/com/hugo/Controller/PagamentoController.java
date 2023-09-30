c08daf7ad15f3758b1af45333af68d2e5c17a7e6
Hugomes99 <hugochristian99@gmail.com>
2023-09-29 22:04:46 -0300

    Ajustes no Controller

--- src/main/java/com/hugo/Controller/PagamentoController.java
+++ src/main/java/com/hugo/Controller/PagamentoController.java
@@ -2,49 +2,63 @@ package com.hugo.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hugo.exeception.ResourceNotFoundException;
 import com.hugo.model.Pagamento;
-import com.hugo.service.PagamentoService;
+import com.hugo.repository.PagamentoRepository;
 
-import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/pagamento")
-@AllArgsConstructor
+
 public class PagamentoController {
 
-    private final PagamentoService pagamentoService;
+    
+    private final PagamentoRepository pagamentoRepository;
+
+    public PagamentoController(PagamentoRepository pagamentoRepository) {
+        this.pagamentoRepository = pagamentoRepository;
+    }
 
     @GetMapping
-    public List<Pagamento> listPagamento(@RequestBody Pagamento pagamento) {
-        return pagamentoService.list(pagamento);
+    public List<Pagamento> getAllPagamentos() {
+        return pagamentoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Pagamento getPagamentoById(@PathVariable Long id) {
+        return pagamentoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Pagamento não encontrado com ID " + id));
     }
 
     @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
     public Pagamento createPagamento(@RequestBody Pagamento pagamento) {
-        return pagamentoService.create(pagamento);
+        return pagamentoRepository.save(pagamento);
     }
 
-    @PutMapping()
-    public ResponseEntity<Pagamento> updatePagamento(@RequestBody Pagamento pagamento) {
-        return pagamentoService.update(pagamento);
+    @PutMapping("/{id}")
+    public Pagamento updatePagamento(@PathVariable Long id, @RequestBody Pagamento updatedPagamento) {
+        return pagamentoRepository.findById(id)
+                .map(pagamento -> {
+                    pagamento.setMetodoPagamento(updatedPagamento.getMetodoPagamento());
+                    pagamento.setValor(updatedPagamento.getValor());
+                    return pagamentoRepository.save(pagamento);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("\r\n" + //
+                        "Pagamento não encontrado com ID " + id));
     }
 
-    @DeleteMapping()
-    public ResponseEntity<Void> deletePagamento(@RequestBody Pagamento pagamento) {
-        return pagamentoService.delete(pagamento);
+    @DeleteMapping("/{id}")
+    public void deletePagamento(@PathVariable Long id) {
+        pagamentoRepository.deleteById(id);
     }
-
 }
