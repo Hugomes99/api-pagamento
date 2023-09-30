a7b80bd88456befcd782b5c3f8a398c5154c160e
moises.costa <moises.costa@leadstation.com.br>
2023-09-30 11:13:02 -0300

    - escrever a interface da service  interface é basicamente um contrato de uso, nenhuma outra classe pode usar a classe de implementação da service, só o interface, ai fazer inserção do @Resource para pegar a classe de implementação na hora de fazer a compilação do arquivo, como so tem um arquivo fazendo a implementação da service não precisamos fazer mais declaração o spring ja faz automático
     como a interface funciona:
      ele é um contrato que mostra o quais funções voce pode usar de uma classe, assim o que ela precisa para funcionar e o que ela retorna
      - quem implementar é obrigo a colocar todos os métodos dentro, pode criar métodos novos, porem ele não vão ser exibidos para quem utilizar , desse modo so se cria métodos privados para para não repetir codigo nos outros métodos
      - que usa só ver os métodos declarados dentro dela

---
+++ src/main/java/com/hugo/service/PagamentoService.java
@@ -0,0 +1,21 @@
+package com.hugo.service;
+
+import java.util.List;
+
+import com.hugo.model.Pagamento;
+import com.hugo.model.PagamentoStatus;
+
+public interface PagamentoService {
+
+         Pagamento createPayment(Pagamento pagamento);
+
+         Pagamento updatePaymentStatus(Long pagamentoId, PagamentoStatus newStatus);
+
+         List<Pagamento> getAllPagamento();
+
+         Pagamento searchPagamentoByCodigo(Long codigo);
+
+         List<Pagamento> searchPagamentoByCpfCnpj(String cpfCnpj);
+
+         List<Pagamento> searchPagamentoByStatus(String pagamentoStatus);
+}
